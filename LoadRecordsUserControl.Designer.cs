ea68bc95ab799ac55bddc458267bd30e9c28de8e
godcandidate <godcandidate@gmail.com>
2023-04-01 23:38:13 +0000

    LoadRecordsUserControl.Designer.csv

--- LoadRecordsUserControl.Designer.cs
+++ LoadRecordsUserControl.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvrecords = new Guna.UI2.WinForms.Guna2DataGridView();
             this.cmbSearchBy = new System.Windows.Forms.ComboBox();
             this.label13 = new System.Windows.Forms.Label();
@@ -40,38 +40,38 @@
             this.ibtnSearch = new FontAwesome.Sharp.IconButton();
             this.ibtnuseDate = new FontAwesome.Sharp.IconButton();
             this.pnlSearchDate = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.btncloseDate = new FontAwesome.Sharp.IconButton();
             this.searchdate = new FontAwesome.Sharp.IconButton();
             this.cmbDateBy = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtWord = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvrecords)).BeginInit();
             this.pnlSearchDate.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvrecords
             // 
-            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
-            this.dgvrecords.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvrecords.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvrecords.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvrecords.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvrecords.ColumnHeadersHeight = 30;
             this.dgvrecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvrecords.DefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvrecords.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvrecords.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvrecords.Location = new System.Drawing.Point(3, 111);
             this.dgvrecords.Name = "dgvrecords";
@@ -211,17 +211,24 @@
             // 
             // pnlSearchDate
             // 
+            this.pnlSearchDate.Controls.Add(this.dateTimePicker1);
             this.pnlSearchDate.Controls.Add(this.btncloseDate);
             this.pnlSearchDate.Controls.Add(this.searchdate);
             this.pnlSearchDate.Controls.Add(this.cmbDateBy);
             this.pnlSearchDate.Controls.Add(this.label2);
             this.pnlSearchDate.Controls.Add(this.label3);
-            this.pnlSearchDate.Controls.Add(this.txtWord);
             this.pnlSearchDate.Location = new System.Drawing.Point(3, 3);
             this.pnlSearchDate.Name = "pnlSearchDate";
             this.pnlSearchDate.Size = new System.Drawing.Size(308, 102);
             this.pnlSearchDate.TabIndex = 34;
             this.pnlSearchDate.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(111, 44);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(186, 20);
+            this.dateTimePicker1.TabIndex = 35;
             // 
             // btncloseDate
             // 
@@ -261,26 +268,21 @@
             this.searchdate.TabIndex = 35;
             this.searchdate.Text = "Search";
             this.searchdate.UseVisualStyleBackColor = false;
+            this.searchdate.Click += new System.EventHandler(this.searchdate_Click);
             // 
             // cmbDateBy
             // 
             this.cmbDateBy.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbDateBy.FormattingEnabled = true;
             this.cmbDateBy.Items.AddRange(new object[] {
-            "Product Type",
-            "Loading Depot",
-            "Discharge date",
-            "Discharge time",
-            "Marker certificate",
-            "Invoice number",
-            "BRV number",
-            "Compartment number",
-            "Contains_water",
-            "Drive name"});
+            "Year",
+            "Month and Year",
+            "Date, Month and Year"});
             this.cmbDateBy.Location = new System.Drawing.Point(111, 6);
             this.cmbDateBy.Name = "cmbDateBy";
             this.cmbDateBy.Size = new System.Drawing.Size(186, 25);
             this.cmbDateBy.TabIndex = 31;
+            this.cmbDateBy.SelectedIndexChanged += new System.EventHandler(this.cmbDateBy_SelectedIndexChanged);
             // 
             // label2
             // 
@@ -303,14 +305,6 @@
             this.label3.Size = new System.Drawing.Size(45, 17);
             this.label3.TabIndex = 32;
             this.label3.Text = "Value";
-            // 
-            // txtWord
-            // 
-            this.txtWord.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWord.Location = new System.Drawing.Point(111, 42);
-            this.txtWord.Name = "txtWord";
-            this.txtWord.Size = new System.Drawing.Size(186, 23);
-            this.txtWord.TabIndex = 33;
             // 
             // LoadRecordsUserControl
             // 
@@ -353,6 +347,6 @@
         public System.Windows.Forms.ComboBox cmbDateBy;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        public System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
