978608bdf64c7bf8f7676676bb9be45f1edd6759
godcandidate <godcandidate@gmail.com>
2023-05-01 17:25:05 +0000

    update LoadRecords.cs added a print method to print a selected record from datagrid

--- LoadRecords.cs
+++ LoadRecords.cs
@@ -30,7 +30,6 @@ namespace GoilRecords
         }
 
 
-
         /// <summary>
         /// Fills the datagrid dgvrecord with logged in user's records table from the database
         /// </summary>
@@ -71,6 +70,40 @@ namespace GoilRecords
 
                 MessageBox.Show("Something went wrong", "View Error");
             }
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //printing a separator 
+            Graphics g = e.Graphics;
+            int startX = e.MarginBounds.Left;
+            int endX = e.MarginBounds.Right;
+            int y = 80;
+
+            e.Graphics.DrawString("Goil Record System", new Font("Georgia", 12), Brushes.Red, new Point(100, 10));
+            e.Graphics.DrawString("Printed by " + _user.Username, new Font("Century Gothic", 10), Brushes.Red, new Point(10, 40));
+            e.Graphics.DrawString("Printed on " + DateTime.Now, new Font("Century Gothic", 10), Brushes.Red, new Point(10, 60));
+
+            Pen separatorPen = new Pen(Color.Black, 2);
+            g.DrawLine(separatorPen, startX, y, endX, y);
+            int count = 100;
+
+            //looping through the data to print each 
+            for (int i=0; i<=17; i++)
+            {
+                e.Graphics.DrawString(loadRecordsUserControl1.dgvrecords.Columns[i].HeaderText + "  : " +
+                    loadRecordsUserControl1.dgvrecords.SelectedRows[0].Cells[i].Value.ToString(), 
+                    new Font("Century Gothic", 12), Brushes.Black, new Point(40, count));
+                count += 20;
+            }
+        }
+
+        private void ibtnprint_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Suit Detail", 400, 500);
+            printPreviewDialog1.ShowDialog();
 
         }
     }
