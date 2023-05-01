c6cc24bbc31bd48f2d9077514097f1f91bd50bfa
godcandidate <godcandidate@gmail.com>
2023-05-01 17:26:17 +0000

    update LoadRecords.Designer.cs added PrintDocumnet and printPreviewDialog

--- LoadRecords.Designer.cs
+++ LoadRecords.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadRecords));
             this.ibtnprint = new FontAwesome.Sharp.IconButton();
             this.iconButton1 = new FontAwesome.Sharp.IconButton();
             this.ibtnView = new FontAwesome.Sharp.IconButton();
             this.loadRecordsUserControl1 = new GoilRecords.LoadRecordsUserControl();
             this.ibtnEditRecord = new FontAwesome.Sharp.IconButton();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.SuspendLayout();
             // 
             // ibtnprint
@@ -54,6 +57,7 @@
             this.ibtnprint.TabIndex = 34;
             this.ibtnprint.Text = "Print Record";
             this.ibtnprint.UseVisualStyleBackColor = false;
+            this.ibtnprint.Click += new System.EventHandler(this.ibtnprint_Click);
             // 
             // iconButton1
             // 
@@ -126,6 +130,20 @@
             this.ibtnEditRecord.UseVisualStyleBackColor = false;
             this.ibtnEditRecord.Click += new System.EventHandler(this.ibtnEditRecord_Click);
             // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
             // LoadRecords
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -149,5 +167,7 @@
         private FontAwesome.Sharp.IconButton ibtnView;
         internal LoadRecordsUserControl loadRecordsUserControl1;
         private FontAwesome.Sharp.IconButton ibtnEditRecord;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
